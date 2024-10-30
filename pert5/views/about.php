a7f875b73906c44cf103bb311cf88cd1e5643088
Rijalul Fikri <fikri.desertlion@gmail.com>
2024-10-30 20:57:55 +0700

    copy base file from pert4
    copy base file from pert4, I'll add more soon especially for the markdown file

---
+++ pert5/views/about.php
@@ -0,0 +1,4 @@
+<?php require("partials/header.php"); ?>
+    <h1 class="text-5xl font-bold mb-3">About</h1>
+    <p>This page is <strong>About</strong></p>
+<?php require("partials/footer.php"); ?>
---
+++ pert5/views/home.php
@@ -0,0 +1,6 @@
+<?php require("partials/header.php"); ?>
+
+<h1 class="text-5xl font-bold mb-3">Home</h1>
+<p>This page is <strong>Home</strong></p>
+
+<?php require("partials/footer.php"); ?>
